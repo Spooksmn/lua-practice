2be5c867b1bbc5fce5196064bfc7bad253811875
Spooksmn <82385414+Spooksmn@users.noreply.github.com>
2022-08-30 03:23:08 +0200

    Create brainfuck.lua

---
+++ brainfuck.lua
@@ -0,0 +1,35 @@
+function brainfuckv1()
+local _ = "Players"
+local __ = "LocalPlayer"
+local ___ = "Character"
+local ____ = "HumanoidRootPart"
+local _____ = "CFrame"
+local ______ = CFrame.Angles(0,math.rad(-90),0)
+local ________ = "true"
+local __________ = "false"
+local ______________ = getgenv()
+
+______________._________ = ________
+while ______________._________ == ________ do task.wait()
+    game[_][__][___][____][_____] = ______
+end
+end
+
+function brainfuckv2a()
+local _ = "You"
+local __ = "are"
+local ___ = "very"
+local ____ = "gay"
+local _____ = "!"
+print(_, __, ___, ____.._____)
+end
+
+
+function brainfuckv2b()
+local _ = string.reverse("ouY")
+local __ = string.reverse("era")
+local ___ = string.reverse("yrev")
+local ____ = string.reverse("yag")
+local _____ = string.reverse("!")
+print(_,__,___,____.._____)
+end
