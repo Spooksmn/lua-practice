e776c1e38ec14c5ece8fa1bf931fb1ab0f73732e
Spooks <82385414+Spooksmn@users.noreply.github.com>
2022-09-04 07:51:27 +0200

    Create math.lua

---
+++ math.lua
@@ -0,0 +1,85 @@
+
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+local Window = OrionLib:MakeWindow({Name = "MATH GAME DANIEL RAT", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
+
+
+local Tab = Window:MakeTab({
+	Name = "Addition",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+
+
+
+Tab:AddButton({
+	Name = "1",
+	Callback = function()
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-150, 1.25, -174, 0, 0, -1, 1, 0, 0, 0, -1, 0)
+      		print("button pressed")
+  	end    
+})
+
+Tab:AddButton({
+	Name = "2",
+	Callback = function()
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-150, 1.25, -290, 0, 0, -1, 1, 0, 0, 0, -1, 0)
+      		print("button pressed")
+  	end    
+})
+
+Tab:AddButton({
+	Name = "3",
+	Callback = function()
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-150, 1.25, -401, 0, 0, -1, 1, 0, 0, 0, -1, 0)
+      		print("button pressed")
+  	end    
+})
+
+Tab:AddButton({
+	Name = "4",
+	Callback = function()
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-150, 1.25, -512, 0, 0, -1, 1, 0, 0, 0, -1, 0)
+      		print("button pressed")
+  	end    
+})
+
+Tab:AddButton({
+	Name = "5",
+	Callback = function()
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-150, 1.25, -734, 0, 0, -1, 1, 0, 0, 0, -1, 0)
+      		print("button pressed")
+  	end    
+})
+
+Tab:AddButton({
+	Name = "6",
+	Callback = function()
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-150, 1.25, -845, 0, 0, -1, 1, 0, 0, 0, -1, 0)
+      		print("button pressed")
+  	end    
+})
+
+Tab:AddButton({
+	Name = "7",
+	Callback = function()
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-150, 1.25, -845, 0, 0, -1, 1, 0, 0, 0, -1, 0)
+      		print("button pressed")
+  	end    
+})
+
+
+Tab:AddButton({
+	Name = "8",
+	Callback = function()
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-150, 1.25, -956, 0, 0, -1, 1, 0, 0, 0, -1, 0)
+      		print("button pressed")
+  	end    
+})
+
+Tab:AddButton({
+	Name = "9",
+	Callback = function()
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-150, 1.25, -1067, 0, 0, -1, 1, 0, 0, 0, -1, 0)
+      		print("button pressed")
+  	end    
+})
